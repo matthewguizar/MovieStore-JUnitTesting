10e917fcd2e83444b9b1f601b1088ae1c6082a11
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-21 09:45:34 -0700

    movie added test

---
+++ src/test/StoreTest.java
@@ -0,0 +1,33 @@
+package src.test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Movie;
+import src.main.models.Store;
+
+public class StoreTest {
+    Store store;
+
+    @Before
+    public void setup(){
+        store = new Store();    
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
+
+    @Test
+    public void movieAdded(){
+        assertTrue(store.contains(new Movie("The Godfather", "Blue-Ray", 9.1)));
+    }
+
+    @Test
+    public void sellMovieTest(){
+        store.sellMovie("The Shawshank Redemption");
+        assertFalse(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2)));
+    }
+
+}
